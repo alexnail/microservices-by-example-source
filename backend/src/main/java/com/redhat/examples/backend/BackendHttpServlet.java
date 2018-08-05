aa9c570650c9267d9947cd4d2d2525c6bcb88205
Aleksandrs Fridmans <alexnail@gmail.com>
2018-08-05 22:48:06 +0300

    code cleanup

--- backend/src/main/java/com/redhat/examples/backend/BackendHttpServlet.java
+++ backend/src/main/java/com/redhat/examples/backend/BackendHttpServlet.java
@@ -18,39 +18,35 @@ package com.redhat.examples.backend;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 /**
- * Created by ceposta 
+ * Created by ceposta
  * <a href="http://christianposta.com/blog>http://christianposta.com/blog</a>.
  */
 public class BackendHttpServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("application/json");
 
-        ObjectMapper mapper = new ObjectMapper();
-        String greeting = req.getParameter("greeting");
-
         ResponseDTO response = new ResponseDTO();
-        response.setGreeting(greeting + " from cluster Backend");
+        response.setGreeting(req.getParameter("greeting") + " from cluster Backend");
         response.setTime(System.currentTimeMillis());
         response.setIp(getIp());
 
-        PrintWriter out = resp.getWriter();
-        mapper.writerWithDefaultPrettyPrinter().writeValue(out, response);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writerWithDefaultPrettyPrinter()
+                .writeValue(resp.getWriter(), response);
     }
 
     private String getIp() {
-        String hostname = null;
+        String hostname;
         try {
             hostname = InetAddress.getLocalHost().getHostAddress();
         } catch (UnknownHostException e) {
